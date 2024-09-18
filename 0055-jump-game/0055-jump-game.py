c1bda27ff10905229ee1f1e91bd942c6824e60e3
Harpreet Kaur <79329465+officialHarpreetKaur@users.noreply.github.com>
2024-09-18 21:06:29 +0530

    Time: 758 ms (12.87%), Space: 12.7 MB (45.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Initialize the maximum reachable index
+        max_reach = 0
+
+        # Iterate through the array
+        for i, jump in enumerate(nums):
+            # If the current index cannot be reached, return False
+            if i > max_reach:
+                return False
+
+            # Update the maximum reachable index
+            max_reach = max(max_reach, i + jump)
+
+        # If the last index can be reached, return True
+        return max_reach >= len(nums) - 1
